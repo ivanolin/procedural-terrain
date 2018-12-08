e8788b532125fbacc042f3143900aaea1f1cdb1b
Laura Shub <shublaura@gmail.com>
2018-12-08 02:54:27 -0600

    added menus, player health (though it goes down way to fast, it was just to test restart screen)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame(){
+		StartCoroutine(LoadScene("MainGame"));
+	}
+
+	public void QuitGame(){
+		Application.Quit();
+	}
+
+    public void RestartGame(){
+		StartCoroutine(LoadScene("MainMenu"));
+    }
+
+    public void GameOver(){
+    	StartCoroutine(LoadScene("RestartMenu"));
+    }
+
+    IEnumerator LoadScene(string scene){
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	public float health;
+	int frames;
+
+	// Use this for initialization
+	void Start () {
+		frames = 0;
+		health = 100f;
+		print("Current health: " + health);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		frames++;
+		health -=0.5f;
+		if (frames % 300 == 0){
+			print("Health: " + health);
+		}
+		if(health < 0.0f){
+			StartCoroutine(LoadScene("RestartMenu"));
+		}
+	}
+
+	IEnumerator LoadScene(string scene){
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+}
